8a559a7ec6c48033c57a6ec10f89acad460458b2
Hannah Andrews <handrews0214@gmail.com>
2018-10-11 09:51:06 -0500

    updated text

--- Testing Stuff/src/test/Hangman.java
+++ Testing Stuff/src/test/Hangman.java
@@ -1,51 +1,50 @@
-package main;
-
+package test;
 import java.util.Scanner;
-import java.util.Random;
 
+public class Hangman {
 
-public class Main {
-	private static String[] words = {"courageous", "magnificent", "terrible", "righteous", "superficial" };
+	private static String[] words = {"terminator", "banana", "computer", "cow", "rain", "water" };
 	private static String word = words[(int) (Math.random() * words.length)];
-	private static String replace = new String(new char[word.length()]).replace("\0", "*");
-	private static int attempts = 0;
+	private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+	private static int count = 0;
 
-public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		
-		while (attempts < 7 && replace.contains("*")) {
-			System.out.println("Guess a letter in the word>> ");
-			System.out.println(replace);
-			String guess = input.next();
-			game(guess);}
-		
-		
-}
-public static void game(String guess) {
-	String newReplace = " ";
-	for (int i = 0; i < word.length(); i++) {
-			if(word.charAt(i)==guess.charAt(0)) {
-				newReplace += guess.charAt(0);}
-			else if(replace.charAt(i) != '*') {
-				newReplace+=word.charAt(i);}
-			else {
-				newReplace += "*";}
-			
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (count < 7 && asterisk.contains("*")) {
+			System.out.println("Guess any letter in the word");
+			System.out.println(asterisk);
+			String guess = sc.next();
+			hang(guess);
 		}
-	if (replace.equals(newReplace)) {
-		attempts++;
+		sc.close();
+	}
+
+	public static void hang(String guess) {
+		String newasterisk = "";
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == guess.charAt(0)) {
+				newasterisk += guess.charAt(0);
+			} else if (asterisk.charAt(i) != '*') {
+				newasterisk += word.charAt(i);
+			} else {
+				newasterisk += "*";
 			}
-	else {
-		replace= newReplace;
-		hangmanImage();}
-	if (replace.equals(word)) {
-		System.out.println("Great job!!! The word was " + word);
+		}
+
+		if (asterisk.equals(newasterisk)) {
+			count++;
+			hangmanImage();
+		} else {
+			asterisk = newasterisk;
+		}
+		if (asterisk.equals(word)) {
+			System.out.println("Correct! You win! The word was " + word);
 		}
 	}
-	
+
 	public static void hangmanImage() {
-		if (attempts == 1) {
+		if (count == 1) {
 			System.out.println("Wrong guess, try again");
 			System.out.println();
 			System.out.println();
@@ -54,7 +53,7 @@ public static void game(String guess) {
 			System.out.println("___|___");
 			System.out.println();
 		}
-		if (attempts == 2) {
+		if (count == 2) {
 			System.out.println("Wrong guess, try again");
 			System.out.println("   |");
 			System.out.println("   |");
@@ -65,7 +64,7 @@ public static void game(String guess) {
 			System.out.println("   |");
 			System.out.println("___|___");
 		}
-		if (attempts == 3) {
+		if (count == 3) {
 			System.out.println("Wrong guess, try again");
 			System.out.println("   ____________");
 			System.out.println("   |");
@@ -77,7 +76,7 @@ public static void game(String guess) {
 			System.out.println("   | ");
 			System.out.println("___|___");
 		}
-		if (attempts == 4) {
+		if (count == 4) {
 			System.out.println("Wrong guess, try again");
 			System.out.println("   ____________");
 			System.out.println("   |          _|_");
@@ -89,7 +88,7 @@ public static void game(String guess) {
 			System.out.println("   |");
 			System.out.println("___|___");
 		}
-		if (attempts == 5) {
+		if (count == 5) {
 			System.out.println("Wrong guess, try again");
 			System.out.println("   ____________");
 			System.out.println("   |          _|_");
@@ -101,7 +100,7 @@ public static void game(String guess) {
 			System.out.println("   |");
 			System.out.println("___|___");
 		}
-		if (attempts == 6) {
+		if (count == 6) {
 			System.out.println("Wrong guess, try again");
 			System.out.println("   ____________");
 			System.out.println("   |          _|_");
@@ -113,7 +112,7 @@ public static void game(String guess) {
 			System.out.println("   |          / \\ ");
 			System.out.println("___|___      /   \\");
 		}
-		if (attempts == 7) {
+		if (count == 7) {
 			System.out.println("GAME OVER!");
 			System.out.println("   ____________");
 			System.out.println("   |          _|_");
